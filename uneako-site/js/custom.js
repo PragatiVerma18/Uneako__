1628f727f401aeaeb8b5f4099ecfd6af009707c7
pragati <itispragativerma@gmail.com>
2019-06-09 22:41:29 +0530

    slick caraousel added

--- uneako-site/js/custom.js
+++ uneako-site/js/custom.js
@@ -108,3 +108,40 @@ jQuery(document).ready(function( $ ) {
   });
 
 });
+
+$(document).ready(function(){
+$('.testimonials').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+  });
+
+  $('.testimonials').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
