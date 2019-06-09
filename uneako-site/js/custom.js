fb449ed050a93c71bed2650dc1539c521757bf25
pragati <itispragativerma@gmail.com>
2019-06-10 01:23:19 +0530

    added responsiveness and overlay

--- uneako-site/js/custom.js
+++ uneako-site/js/custom.js
@@ -114,34 +114,35 @@ $('.testimonials').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 2500,
     arrows: true,
     dots: true,
-  });
-
-  $('.testimonials').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
     responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+   
+      ]
   });
-});
+ 
+  });
